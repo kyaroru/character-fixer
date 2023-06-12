3bcab67eb8481282e76abf8c12359f32e0599090
Carol <kyaroru@outlook.com>
2023-06-13 00:43:44 +0800

    add bear!

--- README.md
+++ README.md
@@ -4,12 +4,19 @@ This is a hobby utility tool to convert your garbled mandarin text into proper c
 
 ## Run in your local
 
-First, run the development server:
+First, clone it
+```sh
+git clone git@github.com:kyaroru/character-fixer.git
+```
 
-```bash
+Then, run it!
+
+```sh
 npm run dev
 # or
 yarn dev
 # or
 pnpm dev
 ```
+
+Tada!
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import iconv from "iconv-lite";
 import { useState } from "react";
+import bear1 from "public/images/bear1.png";
 
 export default function Home() {
-  const [input, setInput] = useState("³ÂÞÈÑ¸");
+  const [input, setInput] = useState("ÄãºÃÎÒÊÇ¼ÒÀÖ");
 
   const convertGarbled = (garbled: string) => {
     // Convert the garbled characters to UTF-16 representation
@@ -17,7 +19,8 @@ export default function Home() {
     console.log(`utf16Pairs: ${utf16Pairs}`);
 
     // // Convert UTF-16 pairs to Buffer
-    const result = utf16Pairs?.map((pair, index) => {
+    const result = utf16Pairs
+      ?.map((pair, index) => {
         const buffer = Buffer.from(pair, "hex");
         const decoded = iconv.decode(buffer, "CP936");
         console.log(`decoded ${index}: ${decoded}`);
@@ -34,56 +37,55 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Fix your garbled mandarin text
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <a href="https://github.com/kyaroru/character-fixer">Fix your garbled mandarin text</a>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a href="https://github.com/kyaroru">By @kyaroru</a>
         </div>
       </div>
-
+      <div className="bear max-w-[60%] lg:max-w-[40%]">
+        <Image src={bear1} alt={"bear"} />
+      </div>
       <div className="relative flex place-items-center ">
-          <div>
-            <label
-              htmlFor="original_value"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Garbled Text
-            </label>
-            <textarea
-              inputMode="text"
-              id="original_value"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Enter your garbled text"
-              defaultValue={input}
-              onChange={(e) => {
-                console.log(e.target.value)
-                setInput(e.target.value)
-              }}
-            />
-          </div>
-          <div className="w-4"></div>
-          <div>
-            <label
-              htmlFor="fixed_value"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Fixed Text
-            </label>
-            <textarea
-              inputMode="text"
-              id="fixed_value"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Fixed text"
-              value={result}
-              disabled
-            />
-          </div>
+        <div>
+          <label
+            htmlFor="original_value"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Garbled Text
+          </label>
+          <textarea
+            inputMode="text"
+            id="original_value"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter your garbled text"
+            defaultValue={input}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setInput(e.target.value);
+            }}
+          />
+        </div>
+        <div className="w-4"></div>
+        <div>
+          <label
+            htmlFor="fixed_value"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Fixed Text
+          </label>
+          <textarea
+            inputMode="text"
+            id="fixed_value"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Fixed text"
+            value={result}
+            disabled
+          />
+        </div>
       </div>
-
-      <div>
-        Enjoy!
-      </div>
+      <div className="mt-4 mb-8">Enjoy your day!</div>
     </main>
   );
 }
